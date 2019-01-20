4264fbdb944e0dc16dcfaa52e969880d2450e909
Justin Clark <justclark.jose@gmail.com>
2019-01-20 12:23:38 -0600

    everything functions except for calucations for time

--- app.js
+++ app.js
@@ -8,21 +8,53 @@ var config = {
     messagingSenderId: "147989451466"
   };
   firebase.initializeApp(config);
+  
   var database = firebase.database();
 //On click, this will store values from form into a variable.
-$(".btn").on("click", function(event) {
+$("#submit").on("click", function(event) {
     event.preventDefault();
     
     var nameStored = $("#nameValue").val().trim();
+    var trainStored = $("#firstTrainValue").val().trim();
     var destinationStored = $("#destinationValue").val().trim();
     var frequencyStored = $("#frequencyValue").val().trim();
 
     var trainInfo = {
         trainName: nameStored,
+        firstTrain: trainStored,
         destinationName: destinationStored,
         frequencyName: frequencyStored,
 
     }
     database.ref().push(trainInfo);
+
+    $("#nameValue").val("");
+    $("#firstTrainValue").val("");
+    $("#destinationValue").val("");
+    $("#frequencyValue").val("");
 });
 
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var train = childSnapshot.val().trainName;
+    var first = childSnapshot.val().firstTrain;
+    var destination = childSnapshot.val().destinationName;
+    var frequency = childSnapshot.val().frequencyName;
+    //var minutesAway = moment()
+    //var next = first.add(minutes, frequency);
+    //console.log(next)
+
+    var newTrain =  $("<tr>").append(
+        $("<td>").text(train),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(first),
+
+
+        // $("<td>").text(empRate),
+        // $("<td>").text(empBilled)
+    );
+    $(".body").append(newTrain);
+    
+});
